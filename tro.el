f84dde51efc362d3d4095dd0c29ecdaa622c685b
leo3 <texas.cyberthal@gmail.com>
2019-11-24 21:29:24 +0800

    increment comments version counter

--- tro.el
+++ tro.el
@@ -6,7 +6,7 @@
 ;; Keywords: outlines, files, convenience
 ;; Package-Requires: ((emacs "24.3") (dash "2.16.0") (f "0.20.0"))
 ;; URL: https://github.com/cyberthal/treefactor
-;; Version: 2.0.0
+;; Version: 2.1.3
 
 ;;; Commentary:
 
